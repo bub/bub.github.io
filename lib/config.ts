1e396d33227ec047913b6ca6fbe8720cbd93352c
bub <178513+bub@users.noreply.github.com>
2025-04-04 21:11:38 +0700

    Update

--- lib/config.ts
+++ lib/config.ts
@@ -1,20 +1,19 @@
-import type { TFunction } from 'i18next';
-import { IS_DEV } from './env';
+import type { TFunction } from "i18next";
+import { IS_DEV } from "./env";
+import { DomainRef } from "./hooks/useConfigDomain";
 
 export interface AuthIpfsEndpoint {
   text?: string;
   value: string;
   location?: string;
-  group?: string
+  group?: string;
 }
-
 
 // Definitions here are with the following values -
 //   info: the name of a logo as defined in ../ui/logos, specifically in namedLogos
 //   text: the IPFS endpoint name
 //   value: the IPFS endpoint domain
 //   location: IPFS gateway location
-
 export function createAuthIpfsEndpoints(t: TFunction): AuthIpfsEndpoint[] {
   return [
     {
@@ -85,8 +84,6 @@ export function createAuthIpfsEndpoints(t: TFunction): AuthIpfsEndpoint[] {
   ];
 }
 
-
-
 export interface AuthIpfsPinner {
   text?: string;
   value: string;
@@ -97,22 +94,32 @@ export interface AuthIpfsPinner {
 //   value: the IPFS pinner domain
 export function createAuthIpfsPinner(t: TFunction): AuthIpfsPinner[] {
   return [
+    // for Beta
+    // {
+    //   text: t('Beta'),
+    //   value: 'https://pinning-service.decoo-cloud.cn'
+    // },
+    // for prod
     {
-      text: t<string>('Crust Pinner'),
-      value: 'https://pin.crustcode.com'
+      text: t<string>("Crust Pinner"),
+      value: "https://pin.crustcode.com",
     },
   ];
 }
 
 export const MOBILE_WIDTH = 960;
 
-export const CrustWalletDownUrl = 'https://chrome.google.com/webstore/detail/crust-wallet/jccapkebeeiajkkdemacblkjhhhboiek';
-export const CrustGetCRU = 'https://swap.crustapps.net';
+export const CrustWalletDownUrl =
+  "https://chromewebstore.google.com/detail/crust-wallet-v3/bhnjponmcdginfhpmfnplhoajoolcfhh";
+export const CrustGetCRU = "https://swap.crust.network";
 
 // export const IS_DEV = process.env.NODE_ENV === 'development'
 // beta prod
-export const ShareEarnENV = IS_DEV ? 'beta' : 'prod'
-export const ShareEarnBaseUrl = IS_DEV ? 'https://files-api.decoo.io' : 'https://api.crustfiles.com'
-export const RecaptchKey = IS_DEV ? 'f8bd392f-2ff5-4dc7-bedd-1be9f175885b' : '09ef6613-21f0-4fb6-ab1f-ded9bedb291e'
-export const GA_ID = IS_DEV ? 'G-HH6CWDPZJ2' : 'G-LP5BSW8V06'
-export const GTM_ID = 'GTM-NPZMBPN'
+export const ShareEarnENV = IS_DEV ? "beta" : "prod";
+export const ShareEarnBaseUrl = () =>
+  IS_DEV ? "https://files-api.decoo.io" : `https://api.${DomainRef.value}`;
+export const RecaptchKey = IS_DEV
+  ? "f8bd392f-2ff5-4dc7-bedd-1be9f175885b"
+  : "09ef6613-21f0-4fb6-ab1f-ded9bedb291e";
+export const GA_ID = IS_DEV ? "G-HH6CWDPZJ2" : "G-LP5BSW8V06";
+export const GTM_ID = "GTM-NPZMBPN";
